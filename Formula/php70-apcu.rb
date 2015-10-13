f99d9f97a0f275d8babd4ddbf5c1f77fca4b03a3
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-10-13 12:23:41 +0800

    Add php70-apcu.rb

---
+++ Formula/php70-apcu.rb
@@ -0,0 +1,48 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php70Apcu < AbstractPhp70Extension
+  init
+  homepage "https://pecl.php.net/package/apcu"
+  url "https://pecl.php.net/get/apcu-4.0.7.tgz"
+  sha256 "178a731ee3435b451f1144bc4a63c9d70d4909a2c434e17c6e07db554df90bd6"
+  head "https://github.com/krakjoe/apcu.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "db2840ade45887a067307cbe03284e2b09ed3845dd573709e9909a3e8f70c718" => :el_capitan
+    sha256 "3f61ab5b03891172a79cd3d9e3fae2457bd425caee2d85b91e6e61c636c4dca6" => :yosemite
+    sha256 "69649e22fca115b040fe4b336e349c1dcfa09838c246859165aed131faa4b431" => :mavericks
+  end
+
+  option "with-apc-bc", "Whether APCu should provide APC full compatibility support"
+  depends_on "pcre"
+
+  def install
+    Dir.chdir "apcu-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    args = []
+    args << "--enable-apcu"
+    args << "--enable-apc-bc" if build.with? "apc-bc"
+
+    safe_phpize
+
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig,
+                          *args
+    system "make"
+    prefix.install "modules/apcu.so"
+    write_config_file if build.with? "config-file"
+  end
+
+  def config_file
+    super + <<-EOS.undent
+      apc.enabled=1
+      apc.shm_size=64M
+      apc.ttl=7200
+      apc.mmap_file_mask=/tmp/apc.XXXXXX
+      apc.enable_cli=1
+    EOS
+  end
+end
